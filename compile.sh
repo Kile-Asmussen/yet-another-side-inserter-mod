d19c15bab86b356b567e2953a6678776b354dbf3
Kile Asmussen <kile.asmussen@gmail.com>
2026-09-07 10:09:39 +0200

    .

--- compile.sh
+++ compile.sh
@@ -7,14 +7,18 @@ NAME_VERSION="${NAME}_${VERSION}"
 rm -f $NAME_VERSION.zip
 rm -rf $NAME_VERSION
 
-if [[ $* != *clean* ]]; then
-    git archive --worktree-attributes HEAD --prefix=$NAME_VERSION/ -o $NAME_VERSION.zip
+if [[ $* != *clean* ]] && [[ $* != *uninstall* ]]; then
+    git archive --worktree-attributes HEAD --prefix=$NAME_VERSION/ -o $NAME_VERSION.zip &> /dev/null
 fi
 
-if [[ $* == *install* ]]; then
+if [[ $* == *install* ]] && [[ $* != *uninstall* ]]; then
     cp -f  $NAME_VERSION.zip ~/.factorio/mods/
 fi
 
-if [[ $* == *unzip* ]]; then
+if [[ $* == *unzip* ]] then
     unzip $NAME_VERSION.zip
+fi
+
+if [[ $* == *uninstall* ]]; then
+    rm -f ~/.factorio/mods/$NAME_VERSION.zip
 fi
