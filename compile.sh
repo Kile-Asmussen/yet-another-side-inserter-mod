aee1d071dce46dd8d4225e096460b7b9cab955db
Kile Asmussen <kile.asmussen@gmail.com>
2026-09-07 09:45:34 +0200

    .

--- compile.sh
+++ compile.sh
@@ -4,4 +4,10 @@ VERSION=$(jq -r '.version' < info.json)
 NAME=$(jq -r '.name' < info.json)
 NAME_VERSION="${NAME}_${VERSION}"
 
+rm -f $NAME_VERSION.zip
+
 git archive --worktree-attributes HEAD --prefix=$NAME_VERSION/ -o $NAME_VERSION.zip
+
+if [[ $1 == install ]]; then
+    cp -f  $NAME_VERSION.zip ~/.factorio/mods/
+fi
