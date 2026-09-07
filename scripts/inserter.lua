ac10f49ac7bd787755779b7c1aecd22496c1e6a5
Kile Asmussen <kile.asmussen@gmail.com>
2026-09-07 21:45:23 +0200

    .

--- scripts/inserter.lua
+++ scripts/inserter.lua
@@ -1,175 +1,31 @@
 
 local util = require 'util'
 local math2d = require 'math2d'
+local yasilib = require 'scripts.yasilib'
 
-local number_of_directions = table_size(defines.direction)
-
-local function add_direction(direction, turn)
-    return (direction + turn) % number_of_directions
-end
-
-local function ortho_or_diagonal(direction)
-    return direction % defines.direction.east
-end
-
-local function dot_product(v1, v2)
-    return v1.x * v2.x + v1.y * v2.y
-end
- 
-local pickup_offset = {
-    [defines.direction.north] = { 1, 1, 2, 2 },
-    [defines.direction.northeast] = { 1.1, 1.1, 2.2, 2.2 },
-}
-
-local drop_offset = {
-    [defines.direction.north] = { 0.7, 1.2, 1.7, 2.2 },
-    [defines.direction.northeast] = { 0.8, 1.3, 1.8, 2.3 },
-}
-local direction_vectors = {}
-
-for _, dir in pairs{ 'north', 'northeast', 'east', 'southeast', 'south', 'southwest', 'west', 'northwest' } do
-    dir = defines.direction[dir]
-    direction_vectors[dir] = math2d.position.get_normalised(util.direction_vectors[dir])
-end
-
-
-local function get_drop_offset(inserter)
-    return math2d.position.subtract(inserter.drop_position, inserter.position)
-end
-
-local function get_pickup_offset(inserter)
-    return math2d.position.subtract(inserter.pickup_position, inserter.position)
-end
-
-local function vector_to_direction(vector)
-    local length = math.sqrt(dot_product(vector, vector))
-    local min = length - 0.001
-    local max = length + 0.001
-    for dir, normal in pairs(direction_vectors) do
-        local proj = dot_product(vector, normal)
-        if min < proj and proj < max then return dir end
-    end
-end
-
-local function get_pickup_direction(inserter)
-    return vector_to_direction(get_pickup_offset(inserter)) or inserter.direction
-end
-
-local function get_drop_direction(inserter)
-    return vector_to_direction(get_drop_offset(inserter)) or util.oppositedirection(inserter.direction)
-end
-
-
-local function get_extension_level(inserter)
-    local offset = get_drop_offset(inserter)
-    local direction = vector_to_direction(offset)
-    local distance = math.sqrt(dot_product(offset, offset))
-    local drops = drop_offset[ortho_or_diagonal(direction)]
-    for i=1, #drops do
-        if math.abs(drops[i] - distance) < 0.2 then return i end
-    end
-    return 2
-end
-
-local function get_max_extension_level(prototype)
-    local speed = prototype.get_inserter_extension_speed('normal')
-    if speed < 0.05 then return 2 else return 4 end
-end
-
-local function set_pickup(inserter, direction, level)
-    level = level or get_extension_level(inserter)
-    direction = direction or get_pickup_direction(inserter)
-    local offset = math2d.position.multiply_scalar(direction_vectors[direction], pickup_offset[ortho_or_diagonal(direction)][level])
-    inserter.pickup_position = math2d.position.add(inserter.position, offset)
-end
-
-local function set_drop(inserter, direction, level)
-    level = level or get_extension_level(inserter)
-    direction = direction or get_drop_direction(inserter)
-    local offset = math2d.position.multiply_scalar(direction_vectors[direction], drop_offset[ortho_or_diagonal(direction)][level])
-    inserter.drop_position = math2d.position.add(inserter.position, offset)
-end
-
-local function adjust_inserter_extension(adjustment, inserter, prototype)
-    if not inserter then return end
-    local level = get_extension_level(inserter)
-    local max = get_max_extension_level(prototype)
-    next_level = util.clamp(level + adjustment, 1, max)
-    set_pickup(inserter, nil, next_level)
-    set_drop(inserter, nil, next_level)
-end
-
-
-local function rotate_inserter_pickup(turn, inserter, prototype)
-    if not inserter then return end
-    local new_direction = add_direction(get_pickup_direction(inserter), turn)
-    set_pickup(inserter, new_direction, nil)
-end
-
-local cardinals = {
-    [defines.direction.north] = true,
-    [defines.direction.east] = true,
-    [defines.direction.south] = true,
-    [defines.direction.west] = true,
-}
-
-local function rotate_inserter(turn, inserter, prototype)
-    if not inserter then return end
-
-    local pickup_direction = add_direction(get_pickup_direction(inserter), turn)
-    local drop_direction = add_direction(get_drop_direction(inserter), turn)
-
-    if cardinals[drop_direction] then inserter.direction = util.oppositedirection(drop_direction) end
-
-    set_pickup(inserter, pickup_direction, nil)
-    set_drop(inserter, drop_direction, nil)
-end
-
-local function reset_inserter(inserter, prototype)
-    if not inserter then return end
-    set_pickup(inserter, util.oppositedirection(inserter.direction), 2)
-    set_drop(inserter, inserter.direction, 2)
-end
-
-local function get_inserter_prototype(inserter)
-    if inserter.type == 'inserter' then return inserter.prototype end
-    if inserter.type == 'entity-ghost' and inserter.ghost_type == 'inserter' then return inserter.ghost_prototype end
-end
-
-local function locate_inserter(event)
-    local player = game.get_player(event.player_index)
-
-    if not player.selected then return end
-    
-    local entity = player.selected
-
-    local prototype = get_inserter_prototype(entity)
-
-    if prototype then return entity, prototype end
-end
 
 script.on_event('yasi-reset-inserter-arm', function(event)
-    reset_inserter(locate_inserter(event))
+    yasilib.reset_inserter(yasilib.locate_inserter(event))
 end)
 
 script.on_event('yasi-extend-inserter-arm', function(event)
-    adjust_inserter_extension(locate_inserter(event), 1)
+    yasilib.adjust_inserter_extension(yasilib.locate_inserter(event), 1)
 end)
 
 script.on_event('yasi-retract-inserter-arm', function(event)
-    adjust_inserter_extension(locate_inserter(event), -1)
+    yasilib.adjust_inserter_extension(yasilib.locate_inserter(event), -1)
 end)
 
 script.on_event('yasi-rotate-inserter-pickup-sunwise', function(event)
     local turn = defines.direction.northeast
     if not player.mod_settings['yasi-8-way-rotation'].value then turn = defines.direction.east end
-    rotate_inserter_pickup(turn, locate_inserter(event))
+    yasilib.rotate_inserter_pickup(turn, yasilib.locate_inserter(event))
 end)
 
 script.on_event('yasi-rotate-inserter-pickup-widdershins', function(event)
     local turn = defines.direction.northwest
     if not player.mod_settings['yasi-8-way-rotation'].value then turn = defines.direction.west end
-    rotate_inserter_pickup(turn, locate_inserter(event))
+    yasilib.rotate_inserter_pickup(turn, yasilib.locate_inserter(event))
 end)
 
 script.on_event('yasi-toggle-paste-inserter-directions', function(event)
@@ -188,13 +44,13 @@ end)
 script.on_event(defines.events.on_pre_entity_settings_pasted, function(event)
     local player = game.get_player(event.player_index)
     if player.mod_settings['yasi-paste-inserter-directions'].value then return end
-    if not get_inserter_prototype(event.destination) then return end
+    if not yasilib.get_inserter_prototype(event.destination) then return end
 
     
     local saved = {
-        drop = get_drop_direction(event.destination),
-        pickup = get_pickup_direction(event.destination),
-        level = get_extension_level(event.destination),
+        drop = yasilib.get_drop_direction(event.destination),
+        pickup = yasilib.get_pickup_direction(event.destination),
+        level = yasilib.get_extension_level(event.destination),
     }
 
     storage.saved = storage.saved or {}
@@ -205,7 +61,7 @@ end)
 script.on_event(defines.events.on_entity_settings_pasted, function(event)
     local player = game.get_player(event.player_index)
     if player.mod_settings['yasi-paste-inserter-directions'].value then return end
-    if not get_inserter_prototype(event.destination) then return end
+    if not yasilib.get_inserter_prototype(event.destination) then return end
 
     storage.saved = storage.saved or {}
     storage.saved[event.player_index] = storage.saved[event.player_index] or {}
@@ -213,8 +69,8 @@ script.on_event(defines.events.on_entity_settings_pasted, function(event)
     storage.saved[event.player_index][event.destination.unit_number] = nil
 
     if saved then
-        set_pickup(event.destination, saved.pickup, saved.level)
-        set_drop(event.destination, saved.drop, saved.level)
+        yasilib.set_pickup(event.destination, saved.pickup, saved.level)
+        yasilib.set_drop(event.destination, saved.drop, saved.level)
     end
 end)
 
@@ -223,14 +79,14 @@ script.on_event(defines.events.on_player_rotated_entity, function(event)
     if not player.mod_settings['yasi-8-way-rotation'].value then return end
 
     local inserter = event.entity
-    local prototype = get_inserter_prototype(inserter)
+    local prototype = yasilib.get_inserter_prototype(inserter)
     if not prototype then return end
     local turn = defines.direction.northwest
-    if add_direction(event.previous_direction, defines.direction.east) == inserter.direction then
+    if yasilib.add_direction(event.previous_direction, defines.direction.east) == inserter.direction then
         turn = defines.direction.northeast
     end
     inserter.direction = event.previous_direction
-    rotate_inserter(inserter, get_inserter_prototype(inserter), turn)
+    yasilib.rotate_inserter(inserter, yasilib.get_inserter_prototype(inserter), turn)
 end)
 
 commands.add_command('yasi-fix-inserters', {'command.yasi-fix-inserters'}, function(command)
@@ -243,8 +99,8 @@ commands.add_command('yasi-fix-inserters', {'command.yasi-fix-inserters'}, funct
 
         for _, inserter in pairs(inserters) do
             if inserter.valid then
-                set_pickup(inserter)
-                set_drop(inserter)
+                yasilib.set_pickup(inserter)
+                yasilib.set_drop(inserter)
             end
         end
 
@@ -254,8 +110,8 @@ commands.add_command('yasi-fix-inserters', {'command.yasi-fix-inserters'}, funct
 
         for _, inserter in pairs(inserters) do
             if inserter.valid then
-                set_pickup(inserter)
-                set_drop(inserter)
+                yasilib.set_pickup(inserter)
+                yasilib.set_drop(inserter)
             end
         end
     end
---
+++ scripts/yasilib.lua
@@ -0,0 +1,170 @@
+
+local yasilib = {}
+
+local util = require 'util'
+local math2d = require 'math2d'
+
+yasilib.number_of_directions = table_size(defines.direction)
+
+function yasilib.add_direction(direction, turn)
+    return (direction + turn) % number_of_directions
+end
+
+function yasilib.ortho_or_diagonal(direction)
+    return direction % defines.direction.east
+end
+
+function yasilib.dot_product(v1, v2)
+    return v1.x * v2.x + v1.y * v2.y
+end
+ 
+yasilib.pickup_offset = {
+    [defines.direction.north] = { 1, 1, 2, 2 },
+    [defines.direction.northeast] = { 1.1, 1.1, 2.2, 2.2 },
+}
+
+yasilib.drop_offset = {
+    [defines.direction.north] = { 0.7, 1.2, 1.7, 2.2 },
+    [defines.direction.northeast] = { 0.8, 1.3, 1.8, 2.3 },
+}
+
+yasilib.normalised_direction_vectors = {}
+
+for _, dir in pairs{ 'north', 'northeast', 'east', 'southeast', 'south', 'southwest', 'west', 'northwest' } do
+    dir = defines.direction[dir]
+    yasilib.normalised_direction_vectors[dir] = math2d.position.get_normalised(util.normalised_direction_vectors[dir])
+end
+
+
+function yasilib.get_drop_offset(inserter)
+    return math2d.position.subtract(inserter.drop_position, inserter.position)
+end
+
+function yasilib.get_pickup_offset(inserter)
+    return math2d.position.subtract(inserter.pickup_position, inserter.position)
+end
+
+function yasilib.vector_to_direction(vector)
+    local length = math.sqrt(dot_product(vector, vector))
+    local min = length - 0.001
+    local max = length + 0.001
+    for dir, normal in pairs(normalised_direction_vectors) do
+        local proj = yasilib.dot_product(vector, normal)
+        if min < proj and proj < max then return dir end
+    end
+end
+
+function yasilib.get_pickup_direction(inserter)
+    return yasilib.vector_to_direction(yasilib.get_pickup_offset(inserter))
+        or inserter.direction
+end
+
+function yasilib.get_drop_direction(inserter)
+    return yasilib.vector_to_direction(yasilib.get_drop_offset(inserter))
+        or util.oppositedirection(inserter.direction)
+end
+
+
+function yasilib.get_extension_level(inserter)
+    local offset = yasilib.get_drop_offset(inserter)
+    local direction = yasilib.vector_to_direction(offset)
+    local distance = math.sqrt(yasilib.dot_product(offset, offset))
+    local drops = yasilib.drop_offset[yasilib.ortho_or_diagonal(direction)]
+    for i=1, #drops do
+        if math.abs(drops[i] - distance) < 0.2 then return i end
+    end
+    if distance > yasilib.drop_offset[4] then return 4 end
+    if distance < yasilib.drop_offset[1] then return 1 end
+    return 2
+end
+
+function yasilib.get_max_extension_level(prototype)
+    local speed = prototype.get_inserter_extension_speed('normal')
+    if speed < 0.05 then return 2 else return 4 end
+end
+
+function yasilib.set_pickup(inserter, direction, level)
+    level = level or yasilib.get_extension_level(inserter)
+    direction = direction or yasilib.get_pickup_direction(inserter)
+    local offset = math2d.position.multiply_scalar(
+        yasilib.normalised_direction_vectors[direction],
+        yasilib.pickup_offset[yasilib.ortho_or_diagonal(direction)][level]
+    )
+    inserter.pickup_position = math2d.position.add(inserter.position, offset)
+end
+
+function yasilib.set_drop(inserter, direction, level)
+    level = level or yasilib.get_extension_level(inserter)
+    direction = direction or yasilib.get_drop_direction(inserter)
+    local offset = math2d.position.multiply_scalar(
+        yasilib.normalised_direction_vectors[direction],
+        yasilib.drop_offset[yasilib.ortho_or_diagonal(direction)][level]
+    )
+    inserter.drop_position = math2d.position.add(inserter.position, offset)
+end
+
+function yasilib.adjust_inserter_extension(adjustment, inserter, prototype)
+    if not inserter then return end
+    local level = yasilib.get_extension_level(inserter)
+    local max = yasilib.get_max_extension_level(prototype)
+    next_level = util.clamp(level + adjustment, 1, max)
+    yasilib.set_pickup(inserter, nil, next_level)
+    yasilib.set_drop(inserter, nil, next_level)
+end
+
+
+function yasilib.rotate_inserter_pickup(turn, inserter, prototype)
+    if not inserter then return end
+    local new_direction = yasilib.add_direction(yasilib.get_pickup_direction(inserter), turn)
+    yasilib.set_pickup(inserter, new_direction, nil)
+end
+
+yasilib.cardinals = {
+    [defines.direction.north] = true,
+    [defines.direction.east] = true,
+    [defines.direction.south] = true,
+    [defines.direction.west] = true,
+}
+
+function yasilib.rotate_inserter(turn, inserter, prototype)
+    if not inserter then return end
+
+    local pickup_direction = yasilib.add_direction(yasilib.get_pickup_direction(inserter), turn)
+    local drop_direction = yasilib.add_direction(yasilib.get_drop_direction(inserter), turn)
+
+    if cardinals[drop_direction] then inserter.direction = util.oppositedirection(drop_direction) end
+
+    yasilib.set_pickup(inserter, pickup_direction, nil)
+    yasilib.set_drop(inserter, drop_direction, nil)
+end
+
+function yasilib.reset_inserter(inserter, prototype)
+    if not inserter then return end
+    yasilib.set_pickup(inserter, util.oppositedirection(inserter.direction), 2)
+    yasilib.set_drop(inserter, inserter.direction, 2)
+end
+
+function yasilib.get_inserter_prototype(inserter)
+    if inserter.type == 'inserter' then return inserter.prototype end
+    if inserter.type == 'entity-ghost' and inserter.ghost_type == 'inserter' then return inserter.ghost_prototype end
+end
+
+function yasilib.locate_inserter(event)
+    local player = game.get_player(event.player_index)
+
+    if not player.selected then return end
+    
+    local entity = player.selected
+
+    local prototype = yasilib.get_inserter_prototype(entity)
+
+    if prototype then return entity, prototype end
+end
+
+setmetatable(lib, {
+    __tostring = function() return 'yasilib' end,
+    __index = function(_, k) error('no such key yasilib.' .. k) end,
+    __newindex = function(_, k) error('no such key yasilib.' .. k) end,
+})
+
+return yasilib
